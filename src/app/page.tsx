3e39e6e6ce51a900571e7a273290e09f90e323d2
Jungmin Lee <54784931+BBongDDa@users.noreply.github.com>
2025-04-15 14:05:05 +0900

    feat : topNavMain 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import Head from "next/head";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import NavBarMain from "@/components/common/topNavMain";
+import Link from "next/link";
+import type { Metadata } from "next"; // Metadata 타입 임포트
 
+// Metadata 객체 export
+export const metadata: Metadata = {
+  title: "아이고 - AI 지각방지 솔루션",
+  description: "아이고 - Ai 지각방지 솔루션",
+  icons: {
+    icon: "/imgs/favi-icon.png",
+    shortcut: "/imgs/favi-icon.png",
+    apple: "/imgs/favi-icon.png",
+  },
+};
 
 export default function Home() {
   return (
-<div className="">
-ddssss
-</div>
+    <div>
+      <NavBarMain title="홈화면" link="#"></NavBarMain>
+    </div>
   );
 }
