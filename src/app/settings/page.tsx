d73beaf292cfb9aa082ed291d29f6c144bddba39
Jungmin Lee <54784931+BBongDDa@users.noreply.github.com>
2025-04-15 14:05:54 +0900

    feat : settings, mypage, calendar 라우터 추가

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import NavBar from "@/components/common/topNav";
+import Link from "next/link";
+import type { Metadata } from "next"; // Metadata 타입 임포트
+
+// Metadata 객체 export
+export const metadata: Metadata = {
+  title: "아이고 - AI 지각방지 솔루션",
+  description: "아이고 - Ai 지각방지 솔루션",
+  icons: {
+    icon: "/imgs/favi-icon.png",
+    shortcut: "/imgs/favi-icon.png",
+    apple: "/imgs/favi-icon.png",
+  },
+};
+
+export default function Mypage() {
+  return (
+    <div>
+      <div>
+        <NavBar title="설정" link="settings"></NavBar>
+      </div>
+    </div>
+  );
+}
