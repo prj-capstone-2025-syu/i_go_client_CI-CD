1a2a6a15b85a3d41da497cafb4344cc9d7619114
Jungmin Lee <shsiskd@gmail.com>
2025-05-14 05:00:20 +0900

    feat : 캘린더 더미데이터 추가

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,20 +10,20 @@ export default function Calendar() {
   const events = [
     {
       title: "아침 먹기",
-      start: "2024-03-17",
-      end: "2024-03-17",
+      start: "2025-05-17",
+      end: "2025-05-17",
       color: "#fb8494",
     },
     {
       title: "점심 먹기",
-      start: "2024-03-19",
-      end: "2024-03-20",
+      start: "2025-05-19",
+      end: "2025-05-20",
       color: "#fb8494",
     },
     {
       title: "저녁 먹기",
-      start: "2024-03-23",
-      end: "2024-03-30",
+      start: "2025-05-23",
+      end: "2025-05-30",
       color: "#fb8494",
     },
   ];
