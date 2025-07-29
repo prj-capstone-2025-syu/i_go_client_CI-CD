ce741bb80c439539decc76cf3f4952389604227a
kod0406 <147029200+kod0406@users.noreply.github.com>
2025-07-29 20:27:44 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,14 @@ const nextConfig: NextConfig = {
 
   // eslint 설정
   eslint: {
-    // 경고: 프로덕션 빌드 중 ESLint 오류를 무시합니다.
-    // 이는 임시방편으로만 사용하고, 가능한 한 빨리 오류를 해결해야 합니다.
+
     ignoreDuringBuilds: true,
   },
 
   // devIndicators 설정 추가
   devIndicators: false, // Dev Tools UI 완전히 비활성화
+
+  output: "standalone",
 };
 
 export default nextConfig;
